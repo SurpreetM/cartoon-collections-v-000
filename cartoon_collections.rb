3217c69f225df97921d1b75882c9a0a7723d239b
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-01 17:57:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.each do |item|
+  array.find_the_cheese do |item|
     cheese_types.include?(item)
   end
 end
