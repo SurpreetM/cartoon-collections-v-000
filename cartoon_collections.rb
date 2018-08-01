524609d9b52e96603f7d87e7a80317587aa72489
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-01 17:42:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,9 @@ def summon_captain_planet(planeteer_calls)
 end
 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.include? do |call|
+    call.length > 4
 end
 
 def find_the_cheese# code an argument here
